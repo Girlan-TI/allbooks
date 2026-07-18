111eb539830098dbd98fbd64fecc098aa516bcfa
Girlan Monteiro Moraes <010397549@br-atacadao.corp>
2026-07-18 16:42:07 -0400

    Altera o server

--- src/server.js
+++ src/server.js
@@ -19,5 +19,5 @@ server.use(router) //rotas na prota 3000
 
 server.listen(8000, () => {
   console.log("Boas-vindas a API do AllBooks Alura")
-  console.log("API disponivel através da url http://localhost:8000")
+  console.log("API disponivel em url http://localhost:8000")
 })
